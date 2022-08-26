eff333f1bf4a6ca31d8901e611da4dbe12ff4132
erbridge <f@erbridge.co.uk>
2022-08-26 14:27:34 +0200

    Continue to dump mas installed packages

--- dump.sh
+++ dump.sh
@@ -9,15 +9,15 @@ SCRIPT_PATH=$(
 if [[ "$(uname -s)" == "Darwin" ]]; then
   if [[ "$(uname -m)" == "arm64" ]] || [[ "$(sysctl -in sysctl.proc_translated)" == "1" ]]; then
     if command -v /usr/local/bin/brew >/dev/null 2>&1; then
-      arch -x86_64 /usr/local/bin/brew bundle dump --file="$SCRIPT_PATH/homebrew/Brewfile.intel" --force
+      arch -x86_64 /usr/local/bin/brew bundle dump --file="$SCRIPT_PATH/homebrew/Brewfile.intel" --mas --force
     fi
 
     if command -v /opt/homebrew/bin/brew >/dev/null 2>&1; then
-      arch -arm64e /opt/homebrew/bin/brew bundle dump --file="$SCRIPT_PATH/homebrew/Brewfile" --force
+      arch -arm64e /opt/homebrew/bin/brew bundle dump --file="$SCRIPT_PATH/homebrew/Brewfile" --mas --force
     fi
   else
     if command -v brew >/dev/null 2>&1; then
-      brew bundle dump --file="$SCRIPT_PATH/homebrew/Brewfile" --force
+      brew bundle dump --file="$SCRIPT_PATH/homebrew/Brewfile" --mas --force
       rm "$SCRIPT_PATH/homebrew/Brewfile.intel"
     fi
   fi
